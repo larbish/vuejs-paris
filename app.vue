bb06609c0f2f7ae15ed3fb3829c66f9d0d64618c
Baptiste Leproux <leproux.baptiste@gmail.com>
2023-10-17 14:53:14 +0200

    Revert "content"
    This reverts commit 08b0929d25e752d6911a87e2a87ea1e0479bbf19.

--- app.vue
+++ app.vue
@@ -1,45 +1,5 @@
 <template>
-  <UMain>
-    <UContainer>
-      <UHeader>
-        <template #logo>
-          <span>
-            Studio Demo
-          </span>
-        </template>
-        <template #right>
-          <UColorModeButton />
-        </template>
-      </UHeader>
-
-      <NuxtPage />
-
-      <UFooter>
-        <template #right>
-          <div class="flex items-center justify-center gap-4">
-            <UButton
-              icon="i-mdi-github"
-              :to="`https://github.com/larbish`"
-              color="gray"
-              variant="ghost"
-              target="_blank"
-              :padded="false"
-            />
-            <UButton
-              icon="i-mdi-twitter"
-              :to="`https://twitter.com/_larbish`"
-              color="gray"
-              variant="ghost"
-              target="_blank"
-              :padded="false"
-            />
-          </div>
-        </template>
-      </UFooter>
-    </UContainer>
-  </UMain>
+  <div>
+    <NuxtPage />
+  </div>
 </template>
-
-<script setup lang="ts">
-// const { header, footer, socials } = useAppConfig()
-</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,4 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  extends: '@nuxt/ui-pro',
-  modules: ['@nuxt/content', '@nuxt/ui'],
-  ui: {
-    icons: ['mdi']
-  }
+  modules: ['@nuxt/content']
 })
