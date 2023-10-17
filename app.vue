08b0929d25e752d6911a87e2a87ea1e0479bbf19
Baptiste Leproux <leproux.baptiste@gmail.com>
2023-10-17 13:21:30 +0200

    content

--- app.vue
+++ app.vue
@@ -1,5 +1,45 @@
 <template>
-  <div>
-    <NuxtPage />
-  </div>
+  <UMain>
+    <UContainer>
+      <UHeader>
+        <template #logo>
+          <span>
+            Studio Demo
+          </span>
+        </template>
+        <template #right>
+          <UColorModeButton />
+        </template>
+      </UHeader>
+
+      <NuxtPage />
+
+      <UFooter>
+        <template #right>
+          <div class="flex items-center justify-center gap-4">
+            <UButton
+              icon="i-mdi-github"
+              :to="`https://github.com/larbish`"
+              color="gray"
+              variant="ghost"
+              target="_blank"
+              :padded="false"
+            />
+            <UButton
+              icon="i-mdi-twitter"
+              :to="`https://twitter.com/_larbish`"
+              color="gray"
+              variant="ghost"
+              target="_blank"
+              :padded="false"
+            />
+          </div>
+        </template>
+      </UFooter>
+    </UContainer>
+  </UMain>
 </template>
+
+<script setup lang="ts">
+// const { header, footer, socials } = useAppConfig()
+</script>
